af017444379f77e04aca95df6c3b51c204c7fbd5
izaias <izaias@debian.home.local>
2024-12-04 16:29:42 -0300

    rebild

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // src/index.js
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configuração para arquivos estáticos (como imagens, CSS, JS)
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Rota de teste
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.get('/', (req, res) => {
-  res.send('Bem-vindo ao CRM (crm_new)!');
+  res.render('dashboard');
 });
 
 // Iniciar o servidor
